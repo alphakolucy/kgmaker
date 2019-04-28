947bcb6718f86368d3e0ae1356b34da6845bcf39
alphonso <lingrui_xiong@163.com>
2019-04-28 08:37:48 +0800

    The encyclopedia  page are completed(edit 0.1),百科页面前后端完成

--- src/main/java/com/warmer/kgmaker/dal/impl/EncyclopaediaRepositoryImpl.java
+++ src/main/java/com/warmer/kgmaker/dal/impl/EncyclopaediaRepositoryImpl.java
@@ -11,10 +11,7 @@ import org.neo4j.driver.v1.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 /**
@@ -24,12 +21,12 @@ import java.util.Set;
 public class EncyclopaediaRepositoryImpl implements EncyclopaediaRepository {
 
 
-
     @Autowired
     private Neo4jUtil neo4jUtil;
 
     /**
      * 获取百科信息
+     *
      * @param baiKeName
      * @return
      */
@@ -37,35 +34,122 @@ public class EncyclopaediaRepositoryImpl implements EncyclopaediaRepository {
     public HashMap<String, Object> getBaiKeEntity(String baiKeName) {
         HashMap<String, Object> map = new HashMap<>();
 
-        String cql = String.format("MATCH (n) WHERE n.name='%s' return *",baiKeName);
+        String cql = String.format("MATCH (n) WHERE n.name='%s' return *", baiKeName);
 
 
         StatementResult statementResult = neo4jUtil.excuteCypherSql(cql);
 
         Record record = statementResult.next();
         List<Pair<String, Value>> pairs = record.fields();
-        Map<String, Object> recordMap = record.asMap();
-        List<Value> values = record.values();
-//        Set<Map.Entry<String, Object>> entries = recordMap.entrySet();
-
         for (Pair<String, Value> pair : pairs) {
             String typeName = pair.value().type().name();
-            if ("NODE".equals(typeName)){
+            if ("NODE".equals(typeName)) {
                 Node node = pair.value().asNode();
 
                 Map<String, Object> nodeMap = node.asMap();
                 Set<Map.Entry<String, Object>> entries = nodeMap.entrySet();
                 for (Map.Entry<String, Object> entry : entries) {
-                    map.put(entry.getKey(),entry.getValue());
+                    map.put(entry.getKey(), entry.getValue());
                 }
             }
         }
-//        for (Map.Entry<String, Object> entry : entries) {
-//            String key = entry.getKey();
-//            Object value = entry.getValue().;
-//            System.out.println(key);
-//            System.out.println(value);
-//        }
+
         return map;
+    }
+
+
+    /**
+     * 查询原因与机制
+     *
+     * @param baiKeName
+     * @return
+     */
+    @Override
+    public List<HashMap<String, Object>> getCausesAndMechanisms(String baiKeName) {
+        List<HashMap<String, Object>> list = new ArrayList<>();
+        HashMap<String, Object> map = new HashMap();
+        String cql = String.format("match(a)-[r:原因与机制]-(b) where a.name = '%s' return b", baiKeName);
+        StatementResult statementResult = neo4jUtil.excuteCypherSql(cql);
+        List<HashMap<String, Object>> causesAndMechanisms = getNodeInfoUtil(statementResult);
+        return causesAndMechanisms;
+    }
+
+
+    /**
+     * 查询法医学鉴定
+     *
+     * @param baiKeName
+     * @return
+     */
+    @Override
+    public List<HashMap<String, Object>> getMedicolegalExpertise(String baiKeName) {
+        List<HashMap<String, Object>> list = new ArrayList<>();
+        HashMap<String, Object> map = new HashMap();
+        String cql = String.format("match(a)-[r:法医学鉴定]-(b) where a.name = '%s' return b", baiKeName);
+
+        StatementResult statementResult = neo4jUtil.excuteCypherSql(cql);
+        List<HashMap<String, Object>> medicolegalExpertise = getNodeInfoUtil(statementResult);
+
+
+        return medicolegalExpertise;
+    }
+
+
+    /**
+     * 临床表现
+     *
+     * @param baiKeName
+     * @return
+     */
+    @Override
+    public List<HashMap<String, Object>> getClinicalFeature(String baiKeName) {
+        List<HashMap<String, Object>> list = new ArrayList<>();
+        HashMap<String, Object> map = new HashMap();
+        String cql = String.format("match(a)-[r:临床表现]-(b) where a.name = '%s' return b", baiKeName);
+        StatementResult statementResult = neo4jUtil.excuteCypherSql(cql);
+
+
+
+
+
+        List<HashMap<String, Object>> clinicalFeature = getNodeInfoUtil(statementResult);
+
+
+        return clinicalFeature;
+    }
+
+
+
+    public List<HashMap<String , Object>> getNodeInfoUtil(StatementResult statementResult) {
+        List<HashMap<String, Object>> list = new ArrayList<>();
+
+
+
+        if (statementResult.hasNext()) {
+            List<Record> records = statementResult.list();
+            for (Record record : records) {
+                List<Pair<String, Value>> pairs = record.fields();
+                for (Pair<String, Value> pair : pairs) {
+                    String typeName = pair.value().type().name();
+                    System.out.println(typeName);
+
+                    if ("NODE".equals(typeName)) {
+                        HashMap<String, Object> map = new HashMap<>();
+                        Node node = pair.value().asNode();
+                        Map<String, Object> nodeMap = node.asMap();
+                        Set<Map.Entry<String, Object>> entries = nodeMap.entrySet();
+                        for (Map.Entry<String, Object> entry : entries) {
+                            map.put(entry.getKey(), entry.getValue());
+                        }
+                        list.add(map);
+                    }
+
+                }
+
+
+            }
+        }
+
+        return list;
     }
 }
